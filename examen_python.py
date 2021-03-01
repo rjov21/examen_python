9d763cea75969af93753c8381271be186b9c23c8
Robinson Ordoñez <robinsonor4@gmail.com>
2021-03-01 11:56:33 -0500

    ejercicio_1

--- examen_python.py
+++ examen_python.py
@@ -4,3 +4,16 @@ Created on Mon Mar  1 11:08:14 2021
 
 @author: DIOS BENDITO
 """
+# ejercicio 1
+kilos = int(input('ingrese la cantidad de kilos comprados: '))
+valor = int(input('ingrese el valor del kilo: '))
+if kilos < 2:
+    desccuento = 0
+elif kilos > 2 and kilos < 5:
+    descuento = (kilos * valor) * 0.1
+elif kilos > 5 and kilos < 10:
+    descuento = (kilos * valor) * 0.15
+else:
+    descuento = (kilos * valor) * 0.2
+valor_final = (kilos * valor) - descuento
+print(f'el valor final a pagar es: {valor_final}')
